d18096889c89dbb2e1e5bd8a9751c5e0053acea1
Wallace Júnior <wflj1997@hotmail.com>
2022-09-07 23:55:28 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,36 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import Image from "next/image";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
-const Product: NextPage = () => {
-  const { query } = useRouter();
+const Product: NextPage = () => (
+  <ProductContainer>
+    <ImageContainer>
+      <Image
+        src="https://files.stripe.com/links/MDB8YWNjdF8xTGZaWUVDY2cyeEx1SEVlfGZsX3Rlc3RfZjhOTVdRMXJFWm9WVDhXMDdTNG5YR3pI00PzaIN1BF"
+        width={520}
+        height={480}
+        alt=""
+      />
+    </ImageContainer>
 
-  return <h1>Product {query.id}</h1>;
-};
+    <ProductDetails>
+      <h1>Camiseta X</h1>
+      <span>R$ 79,90</span>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa alias, at
+        laborum magni, provident similique laboriosam nihil corrupti expedita
+        explicabo ducimus, odio id exercitationem consequatur soluta amet atque
+        sapiente inventore!
+      </p>
+
+      <button>Comprar agora</button>
+    </ProductDetails>
+  </ProductContainer>
+);
 
 export default Product;
